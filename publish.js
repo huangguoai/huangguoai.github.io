f77ac66f6ccc2a4fea8bb965713cb33a791f51f3
huangguoai <huangguoaiai@gmail.com>
2026-08-01 16:39:15 +0900

    Update publish.js

--- publish.js
+++ publish.js
@@ -33,7 +33,7 @@ function getRandomString() {
     return result;
 }
 var emails = [
-    '52crdh@gmail.com'
+    'huangguoaiai@gmail.com'
 ];
 
 var urls=[
@@ -51,13 +51,13 @@ for(var i =0;i<urls.length*3;i++){
 }
 
 var otherUrls = [
-	'https://52dh.pages.dev',
-	'https://52dh.github.io/',
+	'https://huangguoai.pages.dev',
+	'https://huangguoai.github.io/',
 ];
 var foreverUrls = [
-	  'https://52crdh.net',
+	  'https://huangguoai.net',
 	JumpPage,
-	'https://52crdh.com',
+	'https://huangguoai.com',
 	
 ];
 var notices = [
@@ -133,7 +133,7 @@ window.onload = function () {
     var newFavoritesElem =  createFieldElem({title: '<font color=red><b>Ctrl+D</b></font> 收藏此页，永不迷路', items: ""});
     mainElem.appendChild(newFavoritesElem);
     // newest urls
-    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问52成人导航'});
+    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问黄果短剧'});
     mainElem.appendChild(newestFieldElem);
 
     // other urls
