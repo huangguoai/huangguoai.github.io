0a31f614c05be4c625075898faa386f5a7d285ca
huangguoai <huangguoaiai@gmail.com>
2026-09-03 20:41:16 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'naquostmx.cc', 
+	'qnhwwptsw.cc', 
 	'zxzddtzt.cc/', 
     'ediayikma.cc/',
 ];                                                                                                                  
